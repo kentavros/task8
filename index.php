bb71367d3633ad815605c8219d54ccec5fd80e56
kentavros <assh4884@gmail.com>
2017-08-08 16:10:57 +0300

    first

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+include ('template/tmp.php');
+?>
---
+++ template/tmp.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Task 8</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <!--<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>-->
+    <!--[endif]-->
+</head>
+<body>
+<nav class="navbar navbar-inverse ">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand " href="#">Task 8</a>
+        </div>
+    </div>
+</nav>
+
+
+<form role="form" class="form-inline text-center" action="index.php" method="post" style="margin-top: 100px; margin-bottom: 100px;">
+    <div class="form-group">
+        <input type="text" class="form-control" name="text" />
+    </div>
+    <div class="form-group">
+        <input class="btn btn-success" type="submit" value="Search" />
+    </div>
+</form>
+
+
+
+
+<footer class="modal-footer navbar-inverse">
+    <div class="container">
+        <a class="navbar-brand" style="float: right" href="#">Task 8 - end</a>
+    </div>
+</footer>
+<script src="js/bootstrap.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<!-- ðÏÓÌÅÄÎÑÑ ËÏÍÐÉÌÑÃÉÑ É ÓÖÁÔÙÊ JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
